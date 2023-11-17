ea54b8b0182183bd7a1d0a6087f1818235145184
MasterEagle23 <ad.eagle.email@gmail.com>
2023-11-17 18:46:40 +0100

    print timestamp

--- logic/strategy.py
+++ logic/strategy.py
@@ -1,12 +1,13 @@
 from models.bet import Bet
 from models.table import Table
+import datetime
 
 
 def decide(table: Table) -> Bet:
     # TODO: Add Poker Logic Here... :)
     we = table.players[table.activePlayer]
 
-    print(f"Cards: {we.cards}")
+    print(f"[{datetime.datetime.now()}]Cards: {we.cards}")
 
     bet_amount = table.minimumBet + (1/table.round) * we.stash
     print(f"{bet_amount}")
