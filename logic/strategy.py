2cdc1ea27398350b388503d6399e1f95a3569d92
MasterEagle23 <ad.eagle.email@gmail.com>
2023-11-18 00:07:27 +0100

    print table

--- logic/strategy.py
+++ logic/strategy.py
@@ -59,5 +59,8 @@ def decide(table: Table) -> Bet:
     print(f"Bet: {bet_amount} = min {table.minimumBet}, "
           + f" round {table.round}, sum {rank_sum}, stack {we.stack}, players {len(table.players)}")
     del we
-    print(dict(table))
+    try:
+        print(dict(table))
+    except Exception:
+        pass
     return Bet(int(bet_amount))
