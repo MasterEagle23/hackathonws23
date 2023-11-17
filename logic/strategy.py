3e27e6049aca46c576e0f26d2ea418ea06de0635
MasterEagle23 <ad.eagle.email@gmail.com>
2023-11-17 18:42:33 +0100

    made player data more accessible

--- logic/strategy.py
+++ logic/strategy.py
@@ -4,10 +4,11 @@ from models.table import Table
 
 def decide(table: Table) -> Bet:
     # TODO: Add Poker Logic Here... :)
+    we = table.players[table.activePlayer]
 
-    print(f"Cards: {table.players[table.activePlayer].cards}")
+    print(f"Cards: {we.cards}")
 
-    bet_amount = table.minimumBet + (1/table.round) * table.activePlayer.stash
+    bet_amount = table.minimumBet + (1/table.round) * we.stash
     print(f"{bet_amount}")
 
     return Bet(int(bet_amount))
