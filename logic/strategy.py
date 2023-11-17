21a98aee61b12c18dd6a8a9efb2bb142f96754b6
MasterEagle23 <ad.eagle.email@gmail.com>
2023-11-17 22:38:16 +0100

    flush

--- logic/strategy.py
+++ logic/strategy.py
@@ -7,17 +7,17 @@ def decide(table: Table) -> Bet:
     print(f"---\n[{datetime.datetime.now()}]\n")
     we = table.players[table.activePlayer]
 
-    card_list = list([])
+    hand_cards = list([])
     for card in we.cards:
-        card_list = card_list + [(card.rank.value, card.suit.value)]
+        hand_cards = hand_cards + [(card.rank.value, card.suit.value)]
 
-    card_list = [card_list[-1], card_list[-2]]
+    hand_cards = [hand_cards[-1], hand_cards[-2]]
 
-    print(f"Cards: {len(card_list)}, {card_list}, ")
+    print(f"Cards: {len(hand_cards)}, {hand_cards}, ")
 
     rank_sum = 0
     print(rank_sum)
-    for card in card_list:
+    for card in hand_cards:
         if card[0] == "J":
             rank_value = 11
         elif card[0] == "Q":
@@ -31,14 +31,17 @@ def decide(table: Table) -> Bet:
         rank_sum = rank_sum + rank_value
         print(rank_sum)
 
-    if card_list[0][0] == card_list[1][0]:
-        rank_sum = rank_value * 10
+    if hand_cards[0][0] == hand_cards[1][0]:
+        rank_sum = rank_sum * 10
+    
+    if hand_cards[0][1] == hand_cards[1][1]:
+        rank_sum = rank_sum * 1.5
 
     if rank_sum < 15:
         bet_amount = 0
     else:
-        bet_amount = table.minimumBet + ((1 / table.round) * (rank_sum / len(card_list)) / 10) * we.stack
-    print(f"Bet: {bet_amount} ({table.minimumBet} + "
-          + f"(1/{table.round}) * ({rank_sum}/{len(card_list)})/10) * {we.stack}")
+        bet_amount = table.minimumBet + ((1 / table.round) * (rank_sum / len(hand_cards)) / 10) * we.stack
+    print(f"Bet: {bet_amount} = {table.minimumBet} + "
+          + f"((1/{table.round}) * ({rank_sum}/{len(hand_cards)})/10) * {we.stack}")
     del we
     return Bet(int(bet_amount))
