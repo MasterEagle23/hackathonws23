8b40aa243303b392dfe374caa7fdb4118eb449da
MasterEagle23 <ad.eagle.email@gmail.com>
2023-11-17 01:58:29 +0100

    inverse of round * stash

--- logic/strategy.py
+++ logic/strategy.py
@@ -5,6 +5,7 @@ from models.table import Table
 def decide(table: Table) -> Bet:
     # TODO: Add Poker Logic Here... :)
 
-    pass
+    bet_amount = table.minimumBet + (1/table.round) * table.activePlayer.stash
+    print(f"{bet_amount}")
 
-    return Bet(0)
+    return Bet(bet_amount)
