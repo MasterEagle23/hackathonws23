424886ca53c7286ad8ff915745b69510b0346df3
MasterEagle23 <ad.eagle.email@gmail.com>
2023-11-17 18:31:22 +0100

    print cards

--- logic/strategy.py
+++ logic/strategy.py
@@ -5,6 +5,8 @@ from models.table import Table
 def decide(table: Table) -> Bet:
     # TODO: Add Poker Logic Here... :)
 
+    print(f"Cards: {table.activePlayer.cards}")
+
     bet_amount = table.minimumBet + (1/table.round) * table.activePlayer.stash
     print(f"{bet_amount}")
 
