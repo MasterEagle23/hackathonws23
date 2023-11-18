49ac5e87320bd3c1ce9d77cc5a4ef1785f7802bd
MasterEagle23 <ad.eagle.email@gmail.com>
2023-11-18 04:04:43 +0100

    fixed crit issue which caused autofold, start of def besthand

--- logic/strategy.py
+++ logic/strategy.py
@@ -1,5 +1,6 @@
 from models.bet import Bet
 import datetime
+from operator import itemgetter
 
 
 def no_of_active_players(players: dict) -> int:
@@ -9,6 +10,48 @@ def no_of_active_players(players: dict) -> int:
             out = out + 1
 
     return out
+
+
+def best_hand(cards: list) -> (dict, int):
+    """
+    :param cards:
+    :return: (best_hand, value)
+
+    Royal Flush: 30939
+    Straight Flush 1/37260
+
+    Flush ()
+    Straight (HV*100 000): 600 000 - 1 400 000
+    Three-of-a-kind (V*10 000): 20 000 - 140 000
+    Two Pair (V*100+V*100)*10: 5000 - 2700
+    Pair (V*100): 200 - 1400
+    High Card (V): 7 - 14
+    """
+    # conv ranks to values
+    for card in cards:
+        if card[0] == "J":
+            card[0] = 11
+        elif card[0] == "Q":
+            card[0] = 12
+        elif card[0] == "K":
+            card[0] = 13
+        elif card[0] == "A":
+            card[0] = 14
+        else:
+            card[0] = int(card[0])
+
+    # sort list by values
+    cards = sorted(cards, key=itemgetter(1))
+
+    # list of values
+    values = []
+    for i in range(len(cards)):
+        values[i] = cards[i][0]
+
+    # check for straight
+    for card in cards:
+        pass
+    # todo here
 
 
 def decide(table: dict) -> Bet:
@@ -66,14 +109,14 @@ def decide(table: dict) -> Bet:
 
     # raise with good hand
     elif hand_value > 50:
-        bet_amount = table.get("minimumBet") + (hand_value / 10) * table.get("minimumRaise") * (1/table.get('round'))
+        bet_amount = table.get("minimumBet") + (hand_value / 10) * table.get("minimumRaise") * (1 / table.get('round'))
 
     # min raise
     else:
         bet_amount = table.get("minimumBet") + table.get("minimumRaise")
 
     # don't raise with a medium hand
-    if bet_amount - we.get('stack') <= 0 and hand_value < 30:
+    if we.get('stack') - bet_amount <= 0 and hand_value < 30:
         bet_amount = 0
         print(f"dont raise all in with mid")
 
