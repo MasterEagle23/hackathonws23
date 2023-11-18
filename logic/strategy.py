8ae68e60133735a57308226650d8ede464de4d6f
MasterEagle23 <ad.eagle.email@gmail.com>
2023-11-18 01:21:04 +0100

    docu and debug

--- logic/strategy.py
+++ logic/strategy.py
@@ -3,21 +3,24 @@ import datetime
 
 
 def decide(table: dict) -> Bet:
+    # debug timestamp
     print(f"---\n[{datetime.datetime.now()}]\n")
     we = table.get("players")[table.get("activePlayer")]
 
     hand_cards = list([])
     for card in we.get('cards'):
         hand_cards = hand_cards + [(card.get('rank'), card.get('suit'))]
+    print(f"hand_cards: {hand_cards}")
 
     common_cards = []
     for card in table.get("communityCards"):
         common_cards = common_cards + [(card.get('rank'), card.get('suit'))]
+    print(f"common_cards: {common_cards}")
 
     hand_cards = [hand_cards[-1], hand_cards[-2]]
+    print(f"Cards: {len(hand_cards)}, H:{hand_cards}, C: {common_cards}")
 
-    print(f"Cards: {len(hand_cards)}, {hand_cards}, ")
-
+    # calc sum of hand
     rank_sum = 0
     print(rank_sum)
     for card in hand_cards:
@@ -42,16 +45,17 @@ def decide(table: dict) -> Bet:
     if hand_cards[0][1] == hand_cards[1][1]:
         rank_sum = rank_sum * 1.1
 
+    # min sum to play
     if rank_sum < 15:
         bet_amount = 0
 
-    # dont raise with only one other player
+    # don't raise with only one other player
     elif len(table.get("players")) < 3:
         bet_amount = table.get("minimumBet")
     else:
         bet_amount = table.get("minimumBet") + ((1 / table.get('round') + len(table.get("players")))
                                                 * (rank_sum / len(hand_cards)) / 10) * we.get('stack')
-        # dont go all in with a medium hand
+        # don't go all in with a medium hand
         if bet_amount - we.get('stack') <= 0 and rank_sum < 30:
             bet_amount = 0
 
